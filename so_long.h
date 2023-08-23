9d293779244ec355e8af85fa7a25be8c8021aaf6
Ellande Medina Seoane <emedina-@c4r2s3.42urduliz.com>
2023-08-23 15:28:34 +0200

    estructurando_el_codigo

--- so_long.h
+++ so_long.h
@@ -1,6 +1,7 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 # include "./libft/libft.h"
+# include "ft_printf/ft_printf.h"
 # include <fcntl.h>
 # include <stdbool.h>
 # include <stdio.h>
@@ -19,12 +20,18 @@ typedef struct s_lib1
 
 }			t_lib1;
 
+
 int			main(int argc, char **argv);
 int			isnt_borded_of_walls(char **map_array, int rows_of_array,
 				int cols_of_array);
-int	hasnt_forbidden_char(char *map_content);
-char	*check_name(char *map_name);
-char	*check_extension(char *map_extension);
+int			hasnt_forbidden_char(char *map_content);
+char		*check_name(char *map_name);
+char		*check_extension(char *map_extension);
+char		*read_the_map(char *map);
+char		*read_map(char *full_path, int i);
+int			how_length_is_the_map(char *full_path);
+char		*join_the_fullpath(char *map_name);
+int			so_long(char *map);
 /* char		*ft_strjoin(char *s1, char *s2);
 int			ft_printf(char const *str, ...);
 char	*get_next_line(int fd); */
